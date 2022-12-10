5bf03b2eabbd38721e45ff404bb2a46a17df1c14
Javad Pournosrat <pournosrat.javad@gmail.com>
2022-12-10 19:46:32 +0330

    initial hash values or first 8 pirme

--- app.py
+++ app.py
@@ -29,6 +29,16 @@ def first_step(in_str):
     
     lst[-int(len(big_endian)):0] = big_endian
     lst = lst[0:512]
-    #print(lst)
+    #print(len(lst))
+
+#HASH values 2, 3, 5, 7, 11, 13, 17, 19
+h0 = 0x6a09e667
+h1 = 0xbb67ae85
+h2 = 0x3c6ef372
+h3 = 0xa54ff53a
+h4 = 0x510e527f
+h5 = 0x9b05688c
+h6 = 0x1f83d9ab
+h7 = 0x5be0cd19
 
 first_step("javad")
