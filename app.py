4af36f98e3d83c7cfb996abb76e94ead54e2b4cd
Javad Pournosrat <pournosrat.javad@gmail.com>
2022-12-12 12:53:19 +0330

    create message_schedule function

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-import numpy as np
+#!/usr/bin/python3
 
 def first_step(in_str):
+    global lst
     binary = ''.join(format(i, '08b') for i in bytearray(in_str, encoding ='utf-8'))
     b = []
     for j in binary:
@@ -8,17 +9,16 @@ def first_step(in_str):
     #print(b)
     
 
-    zeros_array = np.zeros((16, 32))
+    #zeros_array = np.zeros((16, 32))
     
     i = 0
+    
     lst = []
     while i < 512:
         i += 1
         lst.append(int(0))
     #print(lst)
-    
 
-    #print(zeros_array)
     
     lst[0:len(binary)] = b
 
@@ -28,8 +28,23 @@ def first_step(in_str):
     big_endian = format(len(binary), 'b')
     
     lst[-int(len(big_endian)):0] = big_endian
+    
     lst = lst[0:512]
-    #print(len(lst))
+    #print(lst)
+
+def message_schedule():
+    v = 0
+    w = []
+    while v < 1536:
+        v += 1
+        w.append(int(0))
+    s_msg = lst + w
+    #print(len(s_msg))
+
+def 
+
+first_step("javad")
+message_schedule()
 
 #HASH values 2, 3, 5, 7, 11, 13, 17, 19
 h0 = 0x6a09e667
@@ -41,4 +56,3 @@ h5 = 0x9b05688c
 h6 = 0x1f83d9ab
 h7 = 0x5be0cd19
 
-first_step("javad")
