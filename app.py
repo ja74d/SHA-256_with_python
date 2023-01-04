c576236a8173c4d620edb5f96fc9959c7b0840a8
javad pournosrat <pournosrat.javad@gmail.com>
2023-01-04 19:46:34 +0330

    convert list to str for rr and rs

--- app.py
+++ app.py
@@ -48,39 +48,62 @@ first_step("javad")
 
 def rightrotate_7(a):
     global rr_7
+    global str_rr_7
 
+    str_rr_7 = ''
     rr_7 = a[25:32] + a[0:25]
+    for i in rr_7:
+        str_rr_7 = str_rr_7+str(i)
 
 
 def rightrotate_18(a):
     global rr_18
+    global str_rr_18
 
+    str_rr_18 = ''
     rr_18 = a[14:32] + a[0:14]
+    for i in rr_18:
+        str_rr_18 = str_rr_18+str(i)
 
 
 def rightrotate_17(a):
     global rr_17
+    global str_rr_17
 
+    str_rr_17 = ''
     rr_17 = a[15:32] + a[0:15]
-    
+    for i in rr_17:
+        str_rr_17 = str_rr_17+str(i)   
 
 def rightrotate_19(a):
     global rr_19
+    global str_rr_19
 
+    str_rr_19 = ''
     rr_19 = a[13:32] + a[0:13]
+    for i in rr_19:
+        str_rr_19 = str_rr_19+str(i)
 
 #RIGHTSHIFTS
 
 def rightshift_3(a):
     global rs_3
+    global str_rs_3
 
+    str_rs_3 = ''
     rs_3 = [0, 0, 0] + a[0:29]
+    for i in rs_3:
+        str_rs_3 = str_rs_3+str(i)
     
 
 def rightshift_10(a):
     global rs_10
+    global str_rs_10
 
+    str_rs_10 = ''
     rs_10 = rs_3 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] + a[0:22]
+    for i in rs_10:
+        str_rs_10 = str_rs_10+str(i)
     
 
 #STEP TWO
@@ -90,8 +113,13 @@ rightrotate_7(w1)
 rightrotate_18(w1)
 rightshift_3(w1)
 
-#XOR
 
+
+#XOR
+#a = rr_18
+#b = rr_7
+#y=int(a,2) ^ int(b,2)
+#print('{0:b}'.format(y))
 
 
 
